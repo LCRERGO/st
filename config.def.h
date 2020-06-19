932e61e0a295cacb56b4fe670eb26cf890d8c81a
Lucas Cruz dos Reis <lcr.ergo@gmail.com>
2020-06-18 22:39:29 -0300

    Applying scrollback patch

--- config.def.h
+++ config.def.h
@@ -159,12 +159,17 @@ static unsigned int defaultattr = 11;
  */
 static uint forcemousemod = ShiftMask;
 
+/* Determines how much does the mouse scrolls */
+const unsigned int mousescrollincrement = 1;
+
 /*
  * Internal mouse shortcuts.
  * Beware that overloading Button1 will disable the selection.
  */
 static MouseShortcut mshortcuts[] = {
 	/* mask                 button   function        argument       release */
+	{ XK_ANY_MOD,           Button4, kscrollup,      {.i = 1},      0, /* !alt */ -1 },
+	{ XK_ANY_MOD,           Button5, kscrolldown,    {.i = 1},      0, /* !alt */ -1 },
 	{ XK_ANY_MOD,           Button2, selpaste,       {.i = 0},      1 },
 	{ XK_ANY_MOD,           Button4, ttysend,        {.s = "\031"} },
 	{ XK_ANY_MOD,           Button5, ttysend,        {.s = "\005"} },
@@ -188,6 +193,8 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
+	{ TERMMOD,              XK_Up,     kscrollup,      {.i = -1} },
+	{ TERMMOD,              XK_Down,   kscrolldown,    {.i = -1} },
 };
 
 /*
